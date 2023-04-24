1f21f0e90f2cd43e800cec9ee99065f920f0ac6b
Josakko <112590914+Josakko@users.noreply.github.com>
2023-04-24 18:10:58 +0200

    Add files via upload

---
+++ js/about.js
@@ -0,0 +1,18 @@
+const about = document.getElementById("aboutModal");
+const link = document.getElementById("aboutLink");
+const span = document.getElementsByClassName("close")[0];
+
+link.onclick = function() {
+  about.style.display = "block";
+  //about.scrollIntoView({ behavior: "smooth", block: "center" });
+}
+
+span.onclick = function() {
+  about.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == about) {
+    about.style.display = "none";
+  }
+}
